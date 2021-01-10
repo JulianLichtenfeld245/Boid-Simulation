390d8b0f8c58d51372beac7b3e2843b8098a274e
adehoog <62036729+adehoog@users.noreply.github.com>
2021-01-10 17:09:56 -0500

    Update Main.java

--- Engine/Main.java
+++ Engine/Main.java
@@ -1,19 +1,19 @@
 package Engine;
 
-import edu.princeton.cs.introcs.StdDraw;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.Random;
 
 public class Main {
     private static Random rand = new Random();
-    private static int SCREEN_WIDTH = 100;
-    private static int SCREEN_HEIGHT = 100;
+    private static int SCREEN_WIDTH = 512;
+    private static int SCREEN_HEIGHT = 512;
 
     /** adds random number of boids with random positions and velocities to flock */
     private static void addInitialBoids(Flock flock) {
         int numBoids = rand.nextInt(10);
-        int posMax = 100;
-        int velMax = 10;
+        int posMax = 15;
+        int velMax = 1;
         for (int i = 0; i < numBoids; i ++) {
             double xPos = rand.nextInt(posMax + posMax) - posMax;
             double yPos = rand.nextInt(posMax + posMax) - posMax;
@@ -23,21 +23,34 @@ public class Main {
         }
     }
 
-    public static void drawBoids() {
-
+    public static void drawBoids(Flock thisFlock) {
+        double x;
+        double y;
+        for(int i = 0; i < thisFlock.getBoids().size(); i++){
+            x = thisFlock.getBoids().get(i).position.getX()*0.1;
+            y = thisFlock.getBoids().get(i).position.getY()*0.1;
+            StdDraw.picture(x,y,"Engine/boid.png");
+        }
     }
 
     public static void main(String[] args) {
-//        Flock flock = new Flock();
-//        addInitialBoids(flock);
-//        while (true) {
-//            drawBoids();
-//            flock.updateBoids();
-//        }
-        StdDraw.setPenRadius(0.05);
-        StdDraw.setPenColor(StdDraw.BLUE);
-        StdDraw.point(0.5, 0.5);
-        StdDraw.setPenColor(StdDraw.MAGENTA);
-        StdDraw.line(0.2, 0.2, 0.8, 0.2);
+        Flock flock = new Flock();
+        addInitialBoids(flock);
+        /*while (true) {
+            drawBoids();
+            flock.updateBoids();
+        }*/
+
+        StdDraw.setCanvasSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        StdDraw.setScale(-2, +2);
+        StdDraw.enableDoubleBuffering();
+
+        for (double t = 0.0; true; t += 0.02) {
+            StdDraw.clear();
+            drawBoids(flock);
+            flock.updateBoids();;
+            StdDraw.show();
+            StdDraw.pause(30);
+        }
     }
 }
